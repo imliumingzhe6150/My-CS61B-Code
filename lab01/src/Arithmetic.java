5f283bbfe783b071f0693406fc6d8fccbd2ee737
刘名哲 <imliumingzhe@gmail.com>
2026-07-23 21:56:33 +0800

    lab01: Completed Arithmetic.java

--- lab01/src/Arithmetic.java
+++ lab01/src/Arithmetic.java
@@ -1,6 +1,6 @@
 import edu.princeton.cs.algs4.StdIn;
 
-/** Simple Arithmetic Class.
+/** Simple Arithmetic Class.1
  * @author Josh Hug
  * */
 public class Arithmetic {
@@ -20,7 +20,7 @@ public class Arithmetic {
      * @return Sum of a and b
      * */
     public static int sum(int a, int b) {
-        return a * b;
+        return a + b;
     }
 
     public static void main(String[] args) {
