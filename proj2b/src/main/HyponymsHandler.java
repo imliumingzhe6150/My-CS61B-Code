2f0081b6f883d463f145f0df6833e757d291cb70
刘名哲 <imliumingzhe@gmail.com>
2026-08-22 13:29:58 +0800

    。

--- proj2b/src/main/HyponymsHandler.java
+++ proj2b/src/main/HyponymsHandler.java
@@ -1,3 +1,23 @@
 package main;
-public class HyponymsHandler {
+
+import browser.NgordnetQuery;
+import browser.NgordnetQueryHandler;
+import ngrams.NGramMap;
+import wordnet.WordNet;
+
+public class HyponymsHandler extends NgordnetQueryHandler {
+
+    private NGramMap ngm;
+    private WordNet wn;
+
+    public HyponymsHandler(NGramMap ngm, WordNet wn) {
+        this.ngm = ngm;
+        this.wn = wn;
+    }
+
+    @Override
+    public String handle(NgordnetQuery q) {
+        String word = q.words().get(0);
+        return wn.hyponyms(word).toString();
+    }
 }
